c0a41cfe729180358a919b5afb8bef03b902ca83
0jscsshtml <80338568+0jscsshtml@users.noreply.github.com>
2024-03-10 00:39:29 +0800

    Update feature_request_resizable_structure_panel.js

--- Devs/feature_request_resizable_structure_panel.js
+++ Devs/feature_request_resizable_structure_panel.js
@@ -28,37 +28,37 @@
         if ( vueGlobal.$_state.fullAccess ) {
 
             // get Structure Panel and Preview Panel elements
-            const structurePanel = document.getElementById('bricks-structure'),
-				          previewPanel 	 = document.getElementById('bricks-preview');
+            const 	structurePanel = document.getElementById('bricks-structure'),
+					previewPanel 	 = document.getElementById('bricks-preview');
 
             // add attribute for styling      
-			      structurePanel.setAttribute('data-draggable-panel', '');      
+			structurePanel.setAttribute('data-draggable-panel', '');      
 
             // dragging state
             var panelIsResizing = false,
                 panelInitialX   = 0;
 
             // create draggable element
-			      let div = document.createElement('div');
-			      div.id = 'xx-panel-resizable';
-			      structurePanel.prepend(div);
+			let div = document.createElement('div');
+			div.id = 'xx-panel-resizable';
+			structurePanel.prepend(div);
             const dragHandle = structurePanel.children[0];
 
             // set Structure Panel size on page load
             const structurePanelWidth = localStorage.getItem('xx_structure_panel_width');
             
-      			if (structurePanelWidth) {
-              dragHandle.style.right = `${structurePanelWidth - 10}px`;
-              // set Structure Panel width
-      				structurePanel.style.width = `${structurePanelWidth}px`;
-              // update Preview Panel inline style
-      				previewPanel.style.marginRight = `${structurePanelWidth}px`;
-      			}
+			if (structurePanelWidth) {
+				dragHandle.style.right = `${structurePanelWidth - 10}px`;
+				// set Structure Panel width
+      			structurePanel.style.width = `${structurePanelWidth}px`;
+				// update Preview Panel inline style
+      			previewPanel.style.marginRight = `${structurePanelWidth}px`;
+      		}
 
             function updateResize(e) {
                 if (panelIsResizing) {
-                    const offsetX = panelInitialX - e.clientX,
-                          newWidth = Math.max(300, structurePanel.offsetWidth + offsetX); // min-wdth 300px
+                    const 	offsetX = panelInitialX - e.clientX,
+							newWidth = Math.max(300, structurePanel.offsetWidth + offsetX); // min-wdth 300px
 
                     dragHandle.style.right = `${newWidth - 10}px`;
                     structurePanel.style.width = `${newWidth}px`;
@@ -88,21 +88,21 @@
             }
 
             dragHandle.addEventListener('mousedown', (e) => {
-				      e.preventDefault();
+				e.preventDefault();
 
-              // add class on Preview Panel while dragging
-				      previewPanel.classList.add('resizing-panel');
-              // set dragging state to true
-				      panelIsResizing = true;
-              // get initial mouse position
-              panelInitialX = e.clientX;
-				      dragHandle.style.cursor = 'col-resize';
+				// add class on Preview Panel while dragging
+				previewPanel.classList.add('resizing-panel');
+				// set dragging state to true
+				panelIsResizing = true;
+				// get initial mouse position
+				panelInitialX = e.clientX;
+				dragHandle.style.cursor = 'col-resize';
 
                 // call dragging function
-      				document.addEventListener('mousemove', handleMouseMove);
-      				document.addEventListener('mouseup', handleMouseUp);
-      				requestAnimationFrame(() => updateResize(e));
-      			});
+      			document.addEventListener('mousemove', handleMouseMove);
+      			document.addEventListener('mouseup', handleMouseUp);
+      			requestAnimationFrame(() => updateResize(e));
+      		});
 
         }
     })
