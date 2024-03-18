61016d1f59c20f0ed015fafca67fb9f649e04c99
0jscsshtml <80338568+0jscsshtml@users.noreply.github.com>
2024-03-18 11:07:39 +0800

    Update 02-2-feature_request_how_to_copy_attributes.js

--- Devs/Builder_Hacks/02-2-feature_request_how_to_copy_attributes.js
+++ Devs/Builder_Hacks/02-2-feature_request_how_to_copy_attributes.js
@@ -1,8 +1,8 @@
 <style>
-	#bf-copy-attribute.disable, #bf-paste-attribute.disable {
-		pointer-events: none;
-		opacity: 0.35;
-	}
+    #xx-copy-attribute.disable, #xx-paste-attribute.disable {
+        pointer-events: none;
+        opacity: 0.35;
+    }
 </style>
 
 <script>
@@ -13,7 +13,8 @@
         // if user has full access to Builder
         if ( vueGlobal.$_state.fullAccess ) {
             var validKey            = '';
-            const contextMenu       = document.querySelector('.brx-body > #bricks-builder-context-menu');
+            const 	contextMenu     = document.querySelector('.brx-body > #bricks-builder-context-menu'),
+					structurePanel	= document.getElementById('bricks-structure');
 
             // create 'Copy' & 'Paste' Attributes button on Context Menu
             const getIdBtn		= contextMenu.children[0].querySelector('span[data-balloon^="#brxe"]');	
@@ -23,21 +24,44 @@
 				const copyAttrBtn	= getIdBtn.cloneNode();
 				copyAttrBtn.dataset.balloon = balloon;
 				copyAttrBtn.id = id;
+				copyAttrBtn.classList.add('disable');
 				copyAttrBtn.textContent = 'A';
 				return copyAttrBtn;
 			};
 
-			const copyAttrBtn = createAttrBtn('Copy Attribute', 'bf-copy-attribute');
+			const copyAttrBtn = createAttrBtn('Copy Attribute', 'xx-copy-attribute');
 			getIdBtn.parentElement.insertBefore(copyAttrBtn, getIdBtn);
-			const pasteAttrBtn = createAttrBtn('Paste Attribute', 'bf-paste-attribute');
+			const pasteAttrBtn = createAttrBtn('Paste Attribute', 'xx-paste-attribute');
 			getPasteEle.parentElement.insertBefore(pasteAttrBtn, getPasteEle);
 
-            function contextMenuEvents(event) {  
+            function structurePanelEvents() {
+				setTimeout(() => {	
+					const targetId = vueGlobal.$_state.showContextMenu;
+					if ( targetId ) {
+						contextMenuAction(targetId);	
+					}
+				}, 2);
+				
+				function contextMenuAction(targetId) {	
+					if ( !vueGlobal.$_state.activeElement.settings.hasOwnProperty('_attributes') ) {
+						document.getElementById('xx-copy-attribute').classList.add('disable');
+					} else {
+						document.getElementById('xx-copy-attribute').classList.remove('disable');
+					}
+					if ( validKey === '' ) {
+						document.getElementById('xx-paste-attribute').classList.add('disable');
+					} else {
+						document.getElementById('xx-paste-attribute').classList.remove('disable');
+					}
+				}
+			}
+			
+			function contextMenuEvents(event) {  
                 if ( event.type === 'mousedown' && event.isTrusted === true ) {	
                     const targetEle = event.target;
 
                     // copy active element attribute object
-                    if ( targetEle.id === 'bf-copy-attribute' ) {
+                    if ( targetEle.id === 'xx-copy-attribute' ) {
                         event.stopImmediatePropagation();
                         if ( vueGlobal.$_state.activeElement.settings.hasOwnProperty('_attributes') ) {
                             validKey = vueGlobal.$_state.activeElement.settings._attributes[0]['id'];
@@ -49,7 +73,7 @@
                     }
                     
                     // paste attribute object on new element
-                    if ( targetEle.id === 'bf-paste-attribute' ) {
+                    if ( targetEle.id === 'xx-paste-attribute' ) {
                         event.stopImmediatePropagation();
                         vueGlobal.$_readFromClipboard().then((clipboardData) => {
                             const copiedObj 		= clipboardData,
@@ -74,6 +98,7 @@
             };
 
             contextMenu.addEventListener('mousedown', contextMenuEvents);
+			structurePanel.addEventListener('contextmenu', structurePanelEvents);
         };
     })
 </script>
